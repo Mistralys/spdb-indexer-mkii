a265efcb300043e8e6d5eeb5e9affc35e75c2783
Mistralys <s.mordziol@gmail.com>
2022-08-31 12:34:55 +0200

    Added a status bar management utility.

---
+++ Classes/UI/StatusBarManager.cs
@@ -0,0 +1,26 @@
+﻿namespace SPDB_MKII.Classes.UI
+{
+    internal class StatusBarManager
+    {
+        private readonly ToolStripStatusLabel control;
+
+        public StatusBarManager(ToolStripStatusLabel labelControl)
+        {
+            control = labelControl;
+
+            ResetText();
+        }
+
+        public StatusBarManager ShowText(string text)
+        {
+            control.Text = text;
+            return this;
+        }
+
+        public StatusBarManager ResetText()
+        {
+            control.Text = "";
+            return this;
+        }
+    }
+}
