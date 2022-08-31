83a00fed7f627f4c858745cad4bd1828a1e6cd03
Mistralys <s.mordziol@gmail.com>
2022-08-31 12:34:38 +0200

    Added utility to handle stored config JSON.

---
+++ Classes/DelimitedJSONParser.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using SPDB_MKII.Classes.DatabaseInfos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPDB_MKII.Classes
+{
+    internal class DelimitedJSONParser
+    {
+        private readonly List<string> items = new();
+
+        public List<string> Items => items;
+
+        public int Count => Items.Count;
+
+        public DelimitedJSONParser(string source, string delimiter)
+        {
+            string[] list = source.Split(delimiter);
+
+            foreach (string item in list)
+            {
+                items.Add(item.Trim());
+            }
+        }
+    }
+}
