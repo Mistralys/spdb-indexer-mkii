6c9da9429223e225c67d464ef2779e6a556088fc
Mistralys <s.mordziol@gmail.com>
2022-08-31 12:36:25 +0200

    Added folder handling.

--- Forms/Folders.Designer.cs
+++ Forms/Folders.Designer.cs
@@ -28,57 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.MenuBar = new System.Windows.Forms.MenuStrip();
+            this.MenuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuAddFolder = new System.Windows.Forms.ToolStripMenuItem();
+            this.StatusBar = new System.Windows.Forms.StatusStrip();
+            this.LabelStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ListFolders = new System.Windows.Forms.ListView();
+            this.ColLabel = new System.Windows.Forms.ColumnHeader();
+            this.ColExists = new System.Windows.Forms.ColumnHeader();
+            this.ColPath = new System.Windows.Forms.ColumnHeader();
+            this.ContextMenuList = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ContextDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.FolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
             this.tableLayoutPanel1.SuspendLayout();
+            this.MenuBar.SuspendLayout();
+            this.StatusBar.SuspendLayout();
+            this.ContextMenuList.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.menuStrip1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.statusStrip1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.MenuBar, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.StatusBar, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.ListFolders, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(801, 682);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(895, 330);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // menuStrip1
+            // MenuBar
             // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(801, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.MenuBar.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MenuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFile,
+            this.MenuEdit});
+            this.MenuBar.Location = new System.Drawing.Point(0, 0);
+            this.MenuBar.Name = "MenuBar";
+            this.MenuBar.Size = new System.Drawing.Size(895, 28);
+            this.MenuBar.TabIndex = 0;
+            this.MenuBar.Text = "menuStrip1";
             // 
-            // statusStrip1
+            // MenuFile
             // 
-            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.statusStrip1.Location = new System.Drawing.Point(0, 658);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(801, 24);
-            this.statusStrip1.TabIndex = 1;
-            this.statusStrip1.Text = "statusStrip1";
+            this.MenuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuExit});
+            this.MenuFile.Name = "MenuFile";
+            this.MenuFile.Size = new System.Drawing.Size(56, 24);
+            this.MenuFile.Text = "{File}";
+            // 
+            // MenuExit
+            // 
+            this.MenuExit.Name = "MenuExit";
+            this.MenuExit.Size = new System.Drawing.Size(126, 26);
+            this.MenuExit.Text = "{Exit}";
+            this.MenuExit.Click += new System.EventHandler(this.MenuExit_Click);
+            // 
+            // MenuEdit
+            // 
+            this.MenuEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuAddFolder});
+            this.MenuEdit.Name = "MenuEdit";
+            this.MenuEdit.Size = new System.Drawing.Size(59, 24);
+            this.MenuEdit.Text = "{Edit}";
+            // 
+            // MenuAddFolder
+            // 
+            this.MenuAddFolder.Name = "MenuAddFolder";
+            this.MenuAddFolder.Size = new System.Drawing.Size(224, 26);
+            this.MenuAddFolder.Text = "{Add folder}";
+            this.MenuAddFolder.Click += new System.EventHandler(this.MenuAddFolder_Click);
+            // 
+            // StatusBar
+            // 
+            this.StatusBar.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.StatusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LabelStatus});
+            this.StatusBar.Location = new System.Drawing.Point(0, 304);
+            this.StatusBar.Name = "StatusBar";
+            this.StatusBar.Size = new System.Drawing.Size(895, 26);
+            this.StatusBar.SizingGrip = false;
+            this.StatusBar.TabIndex = 1;
+            this.StatusBar.Text = "statusStrip1";
+            // 
+            // LabelStatus
+            // 
+            this.LabelStatus.Name = "LabelStatus";
+            this.LabelStatus.Size = new System.Drawing.Size(88, 20);
+            this.LabelStatus.Text = "{Status text}";
+            // 
+            // ListFolders
+            // 
+            this.ListFolders.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ColLabel,
+            this.ColExists,
+            this.ColPath});
+            this.ListFolders.ContextMenuStrip = this.ContextMenuList;
+            this.ListFolders.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ListFolders.FullRowSelect = true;
+            this.ListFolders.GridLines = true;
+            this.ListFolders.Location = new System.Drawing.Point(3, 31);
+            this.ListFolders.Name = "ListFolders";
+            this.ListFolders.ShowGroups = false;
+            this.ListFolders.Size = new System.Drawing.Size(889, 270);
+            this.ListFolders.TabIndex = 2;
+            this.ListFolders.UseCompatibleStateImageBehavior = false;
+            this.ListFolders.View = System.Windows.Forms.View.Details;
+            // 
+            // ColLabel
+            // 
+            this.ColLabel.Text = "{Label}";
+            this.ColLabel.Width = 240;
+            // 
+            // ColExists
+            // 
+            this.ColExists.Text = "{Exists?}";
+            this.ColExists.Width = 80;
+            // 
+            // ColPath
+            // 
+            this.ColPath.Text = "{Path}";
+            this.ColPath.Width = 560;
+            // 
+            // ContextMenuList
+            // 
+            this.ContextMenuList.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.ContextMenuList.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ContextDelete});
+            this.ContextMenuList.Name = "ContextMenuList";
+            this.ContextMenuList.Size = new System.Drawing.Size(133, 28);
+            // 
+            // ContextDelete
+            // 
+            this.ContextDelete.Name = "ContextDelete";
+            this.ContextDelete.Size = new System.Drawing.Size(132, 24);
+            this.ContextDelete.Tag = "Style:Dangerous";
+            this.ContextDelete.Text = "{Delete}";
+            this.ContextDelete.Click += new System.EventHandler(this.ContextDelete_Click);
             // 
             // Folders
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(801, 682);
+            this.ClientSize = new System.Drawing.Size(895, 330);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.MainMenuStrip = this.MenuBar;
             this.Name = "Folders";
-            this.Text = "Folders";
+            this.Text = "{Manage folders}";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.MenuBar.ResumeLayout(false);
+            this.MenuBar.PerformLayout();
+            this.StatusBar.ResumeLayout(false);
+            this.StatusBar.PerformLayout();
+            this.ContextMenuList.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -86,7 +200,19 @@
         #endregion
 
         private TableLayoutPanel tableLayoutPanel1;
-        private MenuStrip menuStrip1;
-        private StatusStrip statusStrip1;
+        private MenuStrip MenuBar;
+        private StatusStrip StatusBar;
+        private ToolStripMenuItem MenuFile;
+        private ToolStripMenuItem MenuExit;
+        private ToolStripStatusLabel LabelStatus;
+        private ListView ListFolders;
+        private ToolStripMenuItem MenuEdit;
+        private ToolStripMenuItem MenuAddFolder;
+        private FolderBrowserDialog FolderBrowserDialog;
+        private ColumnHeader ColPath;
+        private ColumnHeader ColExists;
+        private ColumnHeader ColLabel;
+        private ContextMenuStrip ContextMenuList;
+        private ToolStripMenuItem ContextDelete;
     }
 }
--- Forms/Folders.cs
+++ Forms/Folders.cs
@@ -1,20 +1,110 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using SPDB_MKII.Classes.FolderInfos;
+using SPDB_MKII.Classes.UI;
+using SPDB_MKII.Forms.Dialogs;
+using SPDB_MKII.Properties;
 
 namespace SPDB_MKII.Forms
 {
     public partial class Folders : Form
     {
+        private readonly StatusBarManager statusBar;
+        private string path = "";
+
         public Folders()
         {
             InitializeComponent();
+
+            statusBar = new StatusBarManager(LabelStatus);
+
+            TranslateUI();
+            RefreshList();
+        }
+
+        private void TranslateUI()
+        {
+            Text = Localization.Dialog_Title_Folders;
+            FolderBrowserDialog.Description = Localization.Dialog_Folders_Browse_Description;
+        }
+
+        private void RefreshList()
+        {
+            ListFolders.Items.Clear();
+
+            foreach(FolderDefinition def in FolderCollection.Folders)
+            {
+                string[] values = { 
+                    def.Label,
+                    def.ExistsPretty,
+                    def.Path
+                };
+
+                ListViewItem item = new(values);
+                item.Tag = def;
+
+                ListFolders.Items.Add(item);
+            }
+        }
+
+        private void MenuExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuAddFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = FolderBrowserDialog.ShowDialog(this);
+
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            path = FolderBrowserDialog.SelectedPath;
+
+            TextPrompt dialog = new();
+
+            dialog.Message = Localization.Dialog_Folders_Message_EnterLabel;
+            dialog.Title = Localization.Dialog_Folders_Title_AddFolder;
+            dialog.Cancelled += Dialog_Cancelled;
+            dialog.Ok += Dialog_Ok;
+
+            dialog.ShowDialog();
+        }
+
+        private void Dialog_Ok(object? sender, Dialogs.TextPromptEvents.OKEventArgs e)
+        {
+            FolderCollection.AddFolderDefinition(path, e.Text);
+            FolderCollection.Save();
+
+            statusBar.ShowText(Localization.Dialog_Folders_Message_Added_Folder);
+
+            RefreshList();
+        }
+
+        private void Dialog_Cancelled(object? sender, EventArgs e)
+        {
+            statusBar.ShowText(Localization.Dialog_Folders_Status_CancelledAdding);
+        }
+
+        private void ContextDelete_Click(object sender, EventArgs e)
+        {
+            if (ListFolders.SelectedItems.Count == 0) {
+                return;
+            }
+
+            foreach(ListViewItem item in ListFolders.SelectedItems)
+            {
+                if(item.Tag is FolderDefinition def)
+                {
+                    FolderCollection.DeleteFolderDefinition(def);
+                }
+            }
+
+            FolderCollection.Save();
+
+            RefreshList();
+
+            statusBar.ShowText(Localization.Dialog_Folders_Status_FoldersDeleted);
         }
     }
 }
