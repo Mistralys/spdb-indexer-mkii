76a9c9effc4c5e08aba594df5297731295376634
Mistralys <s.mordziol@gmail.com>
2022-09-08 15:10:53 +0200

    Handling parent tags.

--- Classes/TagInfos/TagRecord.cs
+++ Classes/TagInfos/TagRecord.cs
@@ -9,6 +9,7 @@ namespace SPDB_MKII.Classes.TagInfos
         public const string ColCategoryID = "category_id";
         public const string ColPrimary = "tag_id";
         public const string ColName = "name";
+        public const string ColParentTagID = "parent_tag_id";
 
         public TagRecord(long id) : base(id)
         {
@@ -26,14 +27,95 @@ namespace SPDB_MKII.Classes.TagInfos
             get => GetColumnInt(ColCategoryID);
         }
 
+        public long ParentTagID
+        {
+            get => GetColumnInt(ColParentTagID);
+            set => SetColumn(ColParentTagID, value);
+        }
+
+        public TagRecord? ParentTag
+        {
+            get
+            {
+                if (HasParent)
+                {
+                    return TagCollection.GetTagByID(ParentTagID);
+                }
+
+                return null;
+            }
+            set
+            {
+                if(value == null)
+                {
+                    if (ParentTag == null) 
+                    { 
+                        return;
+                    }
+
+                    TagRecord previous = ParentTag; 
+
+                    SetColumn(ColParentTagID, null);
+
+                    ParentTagSwitched?.Invoke(this, new TagParentSwitched(this, previous));
+                    return;
+                }
+
+                // Switch the category before setting the parent
+                // tag, because this resets the parent tag.
+                if(value.CategoryID != CategoryID)
+                {
+                    Category = value.Category;
+                }
+
+                TagRecord? old = ParentTag;
+
+                SetColumn(ColParentTagID, value.ID);
+
+                ParentTagSwitched?.Invoke(this, new TagParentSwitched(this, old, value));
+            }
+        }
+
+        public bool HasParent
+        {
+            get => ParentTagID > 0;
+        }
+
+        public List<TagRecord> ChildTags
+        {
+            get
+            {
+                List<TagRecord> list = new();
+
+                foreach(TagRecord record in TagCollection.Tags)
+                {
+                    if(record.ParentTag == this)
+                    {
+                        list.Add(record);
+                    }
+                }
+
+                return list;
+            }
+        }
+
         public TagCategoryRecord Category
         {
             get => TagCollection.GetCategoryByID(CategoryID);
             set
             {
+                if(value.ID == CategoryID)
+                {
+                    return;
+                }
+
                 TagCategoryRecord old = Category;
 
                 SetColumn(ColCategoryID, value.ID);
+                
+                // Remove any parent tag, as this will not be available
+                // in the target category.
+                SetColumn(ColParentTagID, null);
 
                 CategorySwitched?.Invoke(this, new TagCategorySwitchedEventArgs(this, old, value));
             }
@@ -58,5 +140,6 @@ namespace SPDB_MKII.Classes.TagInfos
         }
 
         public event EventHandler<TagCategorySwitchedEventArgs>? CategorySwitched;
+        public event EventHandler<TagParentSwitched>? ParentTagSwitched;
     }
 }
