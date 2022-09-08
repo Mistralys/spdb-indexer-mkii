6d074bd6d657db28a815eb7ad84e7927a260fa0c
Mistralys <s.mordziol@gmail.com>
2022-09-08 15:11:14 +0200

    Event for changing a tag's parent tag.

---
+++ Classes/TagInfos/TagParentSwitched.cs
@@ -0,0 +1,22 @@
+﻿namespace SPDB_MKII.Classes.TagInfos
+{
+    internal class TagParentSwitched
+    {
+        private TagRecord tag;
+        private TagRecord? oldTag;
+        private TagRecord? newTag;
+
+        public TagParentSwitched(TagRecord tagRecord, TagRecord? oldParent, TagRecord newParent)
+        {
+            this.tag = tagRecord;
+            this.oldTag = oldParent;
+            this.newTag = newParent;
+        }
+
+        public TagParentSwitched(TagRecord tagRecord, TagRecord oldParent)
+        {
+            this.tag = tagRecord;
+            this.oldTag = oldParent;
+        }
+    }
+}
