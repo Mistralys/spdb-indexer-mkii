3a43180e6eaec0a3cd559dfab8ffbe0583b3bb95
Mistralys <s.mordziol@gmail.com>
2022-09-28 20:57:16 +0200

    Added tests.

--- UnitTests/Tests/TagInfos/TagsManagerTests.cs
+++ UnitTests/Tests/TagInfos/TagsManagerTests.cs
@@ -1,4 +1,5 @@
 using SPDB_MKII.Classes.TagInfos;
+using SPDB_MKII.Classes.TagInfos.TagEvents;
 using UnitTests.Assets;
 
 namespace UnitTests.Tests.TagInfos
@@ -6,19 +7,71 @@ namespace UnitTests.Tests.TagInfos
     [TestClass]
     public class TagsManagerTests : BaseTest
     {
-        public TagsManagerTests() : base()
-        {
-        }
+        #region _Tests
 
         [TestMethod]
         public void TestAddCategory()
         {
-            SPDB_MKII.Program.Log.Debug("Test: Add category.");
-
             TagCategoryRecord category = TagCollection.AddCategory("Test category");
 
             Assert.IsNotNull(category);
             Assert.AreEqual("Test category", category.Name);
+
+            Assert.IsNotNull(_categoryAddedEventArgs);
+            Assert.AreEqual(_categoryAddedEventArgs.Category, category);
         }
+
+        [TestMethod]
+        public void TestModifyCategory()
+        {
+            TagCategoryRecord category = TagCollection.AddCategory("Test category");
+
+            category.Name = "New name";
+            category.Save();
+
+            Assert.IsNotNull(_categorySavedEventArgs);
+            Assert.AreEqual(_categorySavedEventArgs.Category, category);
+
+            // Reset the collection to fetch a fresh category instance
+            TagCollection.ResetCollection();
+
+            TagCategoryRecord freshCategory = TagCollection.GetCategoryByID(category.ID);
+            Assert.AreNotSame(category, freshCategory);
+            Assert.AreEqual("New name", freshCategory.Name);
+        }
+
+        #endregion
+
+        #region Support methods
+
+        private CategoryAddedEventArgs? _categoryAddedEventArgs = null;
+        private CategorySavedEventArgs? _categorySavedEventArgs = null;
+
+        public TagsManagerTests() : base()
+        {
+            TagCollection.CategoryAdded += Handle_CategoryAdded;
+            TagCollection.CategorySaved += Handle_CategorySaved;
+        }
+
+        private void Handle_CategorySaved(object? sender, CategorySavedEventArgs e)
+        {
+            _categorySavedEventArgs = e;
+        }
+
+        private void Handle_CategoryAdded(object? sender, SPDB_MKII.Classes.TagInfos.TagEvents.CategoryAddedEventArgs e)
+        {
+            _categoryAddedEventArgs = e;
+        }
+
+
+        public new void TearDown()
+        {
+            base.TearDown();
+
+            _categoryAddedEventArgs = null;
+            _categorySavedEventArgs = null;
+        }
+
+        #endregion
     }
 }
